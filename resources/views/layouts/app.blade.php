a906693d797f495662026828b1ceefdfe12e2e1b
Ross Wallace <ross.wallace@stratis-media.com>
2017-11-25 14:26:21 +1100

    Narvar test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,9 @@
         <title>{{config('app.name', 'Iris')}}</title>
     </head>
     <body>
-        @yield('content')
+        @include('inc.navbar')
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
