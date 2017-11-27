56b7cb5c0bf75d4f01841c17f3d56c9aeb8f1099
Ross Wallace <ross.wallace@stratis-media.com>
2017-11-27 14:39:34 +1100

    More styling changes abd fixed up css system

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,21 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+        <!--<link rel="stylesheet" href="{{asset('css/style.css')}}"> Use this if you are using the condenser mix in the webpack.mix file.-->
+        <!--Use these below stylesheets if you need to identify where a particular style is coming from-->
+        <!-- STYLESHEETS -->
+        <link rel="stylesheet" href="{{asset('css/pace.css')}}">
+        <link rel="stylesheet" href="{{asset('css/uniform.css')}}">
+        <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+        <link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
+        <link rel="stylesheet" href="{{asset('css/line-icons.css')}}">
+        <link rel="stylesheet" href="{{asset('css/menu_cornerbox.css')}}">
+        <link rel="stylesheet" href="{{asset('css/waves.css')}}">
+        <link rel="stylesheet" href="{{asset('css/switchery.css')}}">
+        <link rel="stylesheet" href="{{asset('css/meteor.css')}}">
+        <link rel="stylesheet" href="{{asset('css/colors.css')}}">
+        <link rel="stylesheet" href="{{asset('css/custom.css')}}">
+        <!-- ENDSTYLESHEETS -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     </head>
     <body>
