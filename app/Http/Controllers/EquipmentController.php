f789ebbc18daac38280f0a2915260ac179ddf60a
Ross Wallace <ross.wallace@stratis-media.com>
2017-11-28 15:07:25 +1100

    Added index method and file

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class EquipmentController extends Controller
 {
-    //
+    public function index(){
+        $title = 'Equipment | IRIS';
+        return view('pages.equipment.index')->with('title', $title);
+    }
 }
