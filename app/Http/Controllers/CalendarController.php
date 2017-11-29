a4a6de41cb382a970d03a5fc9f3c8f4eacbadff1
Ross Wallace <ross.wallace@stratis-media.com>
2017-11-29 23:48:44 +1100

    Setup initial fleet and calendar by fleet routes

---
+++ app/Http/Controllers/CalendarController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Iris\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CalendarController extends Controller
+{
+    public function index($filter = null){
+        $title = 'Calendar';
+        if (isset($filter) /*&& $filter !== ''*/) {
+            return "<h1>Calendar filtered by $filter coming soon!</h1>";
+        } else {
+            return "<h1>Calendar coming soon!</h1>";
+            //return view('pages.types.datatable')->with('title', $title);
+        }
+    }
+}
---
+++ app/Http/Controllers/FleetController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Iris\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FleetController extends Controller
+{
+    public function vehicles(){
+        $title = 'Vehicles';
+        return view('pages.types.datatable')->with('title', $title);
+    }
+
+    public function vel(){
+        $title = 'Vehicle Equipment Lists';
+        return "<h1>Coming Soon</h1>";
+    //    return view('pages.types.datatable')->with('title', $title);
+    }
+
+    public function bookings(){
+        $title = 'Vehicle Bookings';
+        return "<h1>Coming Soon</h1>";
+    //    return view('pages.types.datatable')->with('title', $title);
+    }
+}
