eb54740540c410edc44c296e7abf0fdc4048eda8
Ross Wallace <ross.wallace@stratis-media.com>
2017-11-29 22:28:49 +1100

    Cleaning up data tables file

--- resources/assets/js/pages/table-data.js
+++ resources/assets/js/pages/table-data.js
@@ -2,9 +2,7 @@ $(document).ready(function() {
 
     // Datatables
 
-    $('#example').dataTable();
-
-    //var table = 
+    //Main Equipment List
     $('#equipment-list').DataTable({
         "columnDefs": [
             { "visible": false, "targets": 2 }
@@ -13,46 +11,12 @@ $(document).ready(function() {
             [2, 'asc']
         ],
         "displayLength": 16,
+        "responsive": true
     });
 
 
-    // Order by the grouping
-    $('#example2 tbody').on('click', 'tr.group', function() {
-        var currentOrder = table.order()[0];
-        if (currentOrder[0] === 2 && currentOrder[1] === 'asc') {
-            table.order([2, 'desc']).draw();
-        } else {
-            table.order([2, 'asc']).draw();
-        }
-    });
 
-    $.fn.isValid = function() {
-        return this[0].checkValidity()
-    }
-
-    var t = $('#example3').DataTable();
-
-    $('#add-row').on('click', function() {
-        if ($("#add-row-form").isValid()) {
-            var name = $('#name-input').val(),
-                position = $('#position-input').val(),
-                age = $('#age-input').val(),
-                date = $('#date-input').val(),
-                salary = $('#salary-input').val();
-            t.row.add([
-                name,
-                position,
-                age,
-                date,
-                '$' + salary
-            ]).draw();
-
-            $('.modal').modal('hide');
-
-            return false;
-        }
-    });
-
+    //Not sure why this is here tbh
     $('.date-picker').datepicker({
         orientation: "top auto",
         autoclose: true
