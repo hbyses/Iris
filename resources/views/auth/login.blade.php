5f7a6285bead8b9644f8b59173561914d02244d3
Ross Wallace <ross.wallace@stratis-media.com>
2017-11-30 21:57:09 +1100

    Updates to styling on auth pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,36 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
+    <script>document.body.classList.add("page-login");</script>
+    <!--<body class="page-login"> Using javascript to set the body class as the <body> sits outside of this blade template-->
+            <main class="page-content">
+                <div class="page-inner">
+                    <div id="main-wrapper">
+                        <div class="row">
+                            <div class="col-md-3 center">
+                                <div class="panel panel-white" id="js-alerts">
+                                    <div class="panel-body">
+                                        <div class="login-box">
+                                            <a href="index.html" style="text-transform: uppercase;" class="logo-name text-lg text-center m-t-xs"><img width="130px" src="{{ asset('img/logo.png') }}" /></a>
+                                            <p class="text-center m-t-md">Please login into your account.</p>
+                                            <form class="m-t-md" method="POST" action="{{ route('login') }}">
+                                            {{ csrf_field() }}
+                                                <div class="form-group">
+                                                    <input type="email" class="form-control" placeholder="Email" required>
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="password" name="password" class="form-control password" placeholder="Password" required>
+                                                </div>
+                                                <button type="submit" class="btn btn-success btn-block">Login</button>
+                                                <a href="/forgotten" class="btn btn-default btn-block m-t-md">Reset Password</a>
+                                            </form>
+                                            <p class="text-center m-t-xs text-sm">IRIS - <a href="https://ses.nsw.gov.au">NSWSES</a></p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+                        </div><!-- Row -->
+                    </div><!-- Main Wrapper -->
+                </div><!-- Page Inner -->
+            </main><!-- Page Content -->
+@endsection            
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,39 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
+    <script>document.body.classList.add("page-register");</script>
+    <!--<body class="page-register"> Using javascript to set the body class as the <body> sits outside of this blade template-->
+            <main class="page-content">
+                <div class="page-inner">
+                    <div id="main-wrapper">
+                        <div class="row">
+                            <div class="col-md-3 center">
+                                <div class="panel panel-white" id="js-alerts">
+                                    <div class="panel-body">
+                                        <div class="register-box">
+                                            <a href="index.html" style="text-transform: uppercase;" class="logo-name text-lg text-center m-t-xs"><img width="130px" src="{{ asset('img/logo.png') }}" /></a>
+                                            <p class="text-center m-t-md">Create an account.</p>
+                                            <form class="m-t-md" method="POST" action="{{ route('register') }}">
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control" placeholder="Name" required>
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="email" class="form-control" placeholder="Email" required>
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="password" name="password" class="password form-control" placeholder="Password" required>
+                                                </div>
+                                                <button type="submit" class="btn btn-success btn-block m-t-xs">Submit</button>
+                                                <p class="text-center m-t-xs text-sm">Already have an account?</p>
+                                                <a href="/login" class="btn btn-default btn-block m-t-xs">Login</a>
+                                            </form>
+                                            <p class="text-center m-t-xs text-sm">IRIS - <a href="https://ses.nsw.gov.au">NSWSES</a></p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+                        </div><!-- Row -->
+                    </div><!-- Main Wrapper -->
+                </div><!-- Page Inner -->
+            </main><!-- Page Content -->
+@endsection  
