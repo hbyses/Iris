95852a0a18c89b7401ffda3def8e6e2c56639076
Ross Wallace <ross.wallace@strat.is>
2017-12-01 13:43:31 +1100

    Register form synced with authcontroller

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,14 +14,33 @@
                                             <a href="index.html" style="text-transform: uppercase;" class="logo-name text-lg text-center m-t-xs"><img width="130px" src="{{ asset('img/logo.png') }}" /></a>
                                             <p class="text-center m-t-md">Create an account.</p>
                                             <form class="m-t-md" method="POST" action="{{ route('register') }}">
-                                                <div class="form-group">
-                                                    <input type="text" class="form-control" placeholder="Name" required>
+                                            {{ csrf_field() }}
+                                                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                                                    <input name="name" type="text" class="form-control" placeholder="Name" value="{{ old('name') }}" required autofocus>
+                                                    @if ($errors->has('name'))
+                                                        <span class="help-block">
+                                                            <strong>{{ $errors->first('name') }}</strong>
+                                                        </span>
+                                                    @endif
                                                 </div>
-                                                <div class="form-group">
-                                                    <input type="email" class="form-control" placeholder="Email" required>
+                                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                                    <input name="email" type="email" class="form-control" placeholder="Email" required>
+                                                    @if ($errors->has('email'))
+                                                        <span class="help-block">
+                                                            <strong>{{ $errors->first('email') }}</strong>
+                                                        </span>
+                                                    @endif
                                                 </div>
-                                                <div class="form-group">
+                                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                                                     <input type="password" name="password" class="password form-control" placeholder="Password" required>
+                                                    @if ($errors->has('password'))
+                                                        <span class="help-block">
+                                                            <strong>{{ $errors->first('password') }}</strong>
+                                                        </span>
+                                                    @endif
+                                                </div>
+                                                <div class="form-group">
+                                                    <input id="password-confirm" placeholder="Confirm Password" type="password" class="form-control" name="password_confirmation" required>
                                                 </div>
                                                 <button type="submit" class="btn btn-success btn-block m-t-xs">Submit</button>
                                                 <p class="text-center m-t-xs text-sm">Already have an account?</p>
