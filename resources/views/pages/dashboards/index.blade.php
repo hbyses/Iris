9586bddbf7751d50b9876f33d5d6c3d3de80d0ea
Unknown <ross.wallace@stratis-media.com>
2017-12-11 19:48:44 +1100

    dashboard buttons

--- resources/views/pages/dashboards/index.blade.php
+++ resources/views/pages/dashboards/index.blade.php
@@ -2,20 +2,69 @@
 
 @section('content')
 <div class="row">
-    <h3>Operations</h3>
-    <div class="col-md-4">
-        <a class="btn-default" href="/dashboards/operations/duties">Duty Dashboard</a>
-    </div>
-    <div class="col-md-4">
-        <a class="btn-default" href="/dashboards/operations/#">Test 1 Dashboard</a>
-    </div>
-    <div class="col-md-4">
-        <a class="btn-default" href="/dashboards/operations/#">Test 2 Dashboard</a>
-    </div>
-    <div class="col-md-4">
-        <a class="btn-default" href="/dashboards/operations/#">Test 3 Dashboard</a>
+    <div class="panel panel-red">
+        <div class="panel-heading">
+            <h1 class="panel-title">Operations</h1>
+        </div>
+        <div class="panel-body">
+            <div class="col-md-3">
+                <a class="btn btn-block btn-default" href="/dashboards/operations/duties">Duty Dashboard</a>
+            </div>
+            <div class="col-md-3">
+                <a class="btn btn-block btn-default" href="/dashboards/operations/#">Test 1 Dashboard</a>
+            </div>
+            <div class="col-md-3">
+                <a class="btn btn-block btn-default" href="/dashboards/operations/#">Test 2 Dashboard</a>
+            </div>
+            <div class="col-md-3">
+                <a class="btn btn-block btn-default" href="/dashboards/operations/#">Test 3 Dashboard</a>
+            </div>
+        </div>
     </div>
 </div>
 
+<div class="row">
+    <div class="panel panel-green">
+        <div class="panel-heading">
+            <h3 class="panel-title">Training</h3>
+        </div>
+        <div class="panel-body">
+            <div class="col-md-3">
+                <a class="btn btn-block btn-default" href="/dashboards/training/skills">Member Skills Dashboard</a>
+            </div>
+            <div class="col-md-3">
+                <a class="btn btn-block btn-default" href="/dashboards/training/gap-analysis">Gap Analysis</a>
+            </div>
+            <div class="col-md-3">
+                <a class="btn btn-block btn-default" href="/dashboards/training/#">Training Dashboard</a>
+            </div>
+            <div class="col-md-3">
+                <a class="btn btn-block btn-default" href="/dashboards/training/#">Training Dashboard</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="panel panel-blue">
+        <div class="panel-heading">
+            <h3 class="panel-title">Logistics</h3>
+        </div>
+        <div class="panel-body">
+            <div class="col-md-3">
+                <a class="btn btn-block btn-default" href="/dashboards/logistics/primary">Duty Dashboard</a>
+            </div>
+            <div class="col-md-3">
+                <a class="btn btn-block btn-default" href="/dashboards/logistics/fleet">Test 1 Dashboard</a>
+            </div>
+            <div class="col-md-3">
+                <a class="btn btn-block btn-default" href="/dashboards/logistics/#">Test 2 Dashboard</a>
+            </div>
+            <div class="col-md-3">
+                <a class="btn btn-block btn-default" href="/dashboards/logistics/#">Test 3 Dashboard</a>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
