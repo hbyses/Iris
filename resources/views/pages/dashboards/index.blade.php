3bfc107396016af802f652b5e3470798ba8ec03f
Ross Wallace <ross.wallace@strat.is>
2017-12-14 17:04:02 +1100

    Dash index changes

--- resources/views/pages/dashboards/index.blade.php
+++ resources/views/pages/dashboards/index.blade.php
@@ -2,69 +2,71 @@
 
 @section('content')
 <div class="row">
-    <div class="panel panel-red">
-        <div class="panel-heading">
-            <h1 class="panel-title">Operations</h1>
-        </div>
-        <div class="panel-body">
-            <div class="col-md-3">
-                <a class="btn btn-block btn-default" href="operations/duties">Current Duties</a>
+    <!--Start Ops Panel-->
+    <div class="col-md-4">
+        <div class="panel">
+            <div class="panel-heading panel-red">
+                <h1 style="color: white;" class="panel-title">Operations</h1>
             </div>
-            <div class="col-md-3">
-                <a class="btn btn-block btn-default" href="operations/#">Test 2 Dash</a>
-            </div>
-            <div class="col-md-3">
-                <a class="btn btn-block btn-default" href="operations/capability">Unit Capability</a>
-            </div>
-            <div class="col-md-3">
-                <a class="btn btn-block btn-default" href="operations/#">Test 3 Dashboard</a>
+            <div class="panel-body" style="padding-bottom: 5px;">
+                <table class="table table-condensed" style="margin-bottom: 0px;">
+                    <tr>
+                        <td style="border-top: none; width: 25%;"><a class="btn btn-block btn-default" href="/dashboards/operations/duties">Current Duties</a></td>
+                        <td style="border-top: none; width: 75%;"><p>This dashboard displays information related to the current incident management team outside of declared events.</p></td>
+                    </tr>
+                    <tr>
+                        <td><a class="btn btn-block btn-default" href="/dashboards/operations/capability">Unit Capability</a></td>
+                        <td><p>This dashboard displays information related to currently operational capability based on key metrics compared against the unit capability plan.</p></td>
+                    </tr>
+                </table>
             </div>
         </div>
     </div>
-</div>
+    <!--End Ops Panel-->
 
-<div class="row">
-    <div class="panel panel-green">
-        <div class="panel-heading">
-            <h3 class="panel-title">Training</h3>
-        </div>
-        <div class="panel-body">
-            <div class="col-md-3">
-                <a class="btn btn-block btn-default" href="/dashboards/training/skills">Member Skills Dashboard</a>
+    <!--Start Training Panel-->
+    <div class="col-md-4">
+        <div class="panel">
+            <div class="panel-heading panel-green">
+                <h1 style="color: white;" class="panel-title">Training</h1>
             </div>
-            <div class="col-md-3">
-                <a class="btn btn-block btn-default" href="/dashboards/training/gap-analysis">Gap Analysis</a>
-            </div>
-            <div class="col-md-3">
-                <a class="btn btn-block btn-default" href="/dashboards/training/#">Training Dashboard</a>
-            </div>
-            <div class="col-md-3">
-                <a class="btn btn-block btn-default" href="/dashboards/training/#">Training Dashboard</a>
+            <div class="panel-body" style="padding-bottom: 5px;">
+            <table class="table table-condensed" style="margin-bottom: 0px;">
+                <tr>
+                    <td style="border-top: none; width: 25%;"><a class="btn btn-block btn-default" href="/dashboards/operations/duties">Current Competencies</a></td>
+                    <td style="border-top: none; width: 75%;"><p>This dashboard displays information related to the current spread and quantity of competencies in the unit.</p></td>
+                </tr>
+                <tr>
+                    <td><a class="btn btn-block btn-default" href="/dashboards/operations/capability">Gap Analysis</a></td>
+                    <td><p>This dashboard displays information related to the gaps in member training according to required core competencies.</p></td>
+                </tr>
+            </table>
             </div>
         </div>
     </div>
-</div>
+    <!--End Training Panel-->
 
-<div class="row">
-    <div class="panel panel-blue">
-        <div class="panel-heading">
-            <h3 class="panel-title">Logistics</h3>
-        </div>
-        <div class="panel-body">
-            <div class="col-md-3">
-                <a class="btn btn-block btn-default" href="/dashboards/logistics/primary">Duty Dashboard</a>
+    <!--Start Logistics Panel-->
+    <div class="col-md-4">
+        <div class="panel">
+            <div class="panel-heading panel-blue">
+                <h1 style="color: white;" class="panel-title">Logistics</h1>
             </div>
-            <div class="col-md-3">
-                <a class="btn btn-block btn-default" href="/dashboards/logistics/fleet">Test 1 Dashboard</a>
-            </div>
-            <div class="col-md-3">
-                <a class="btn btn-block btn-default" href="/dashboards/logistics/#">Test 2 Dashboard</a>
-            </div>
-            <div class="col-md-3">
-                <a class="btn btn-block btn-default" href="/dashboards/logistics/#">Test 3 Dashboard</a>
+            <div class="panel-body" style="padding-bottom: 5px;">
+                <table class="table table-condensed" style="margin-bottom: 0px;">
+                    <tr>
+                        <td style="border-top: none; width: 25%;"><a class="btn btn-block btn-default" href="/dashboards/operations/duties">Dash One</a></td>
+                        <td style="border-top: none; width: 75%;"><p>This dashboard displays information related to the current spread and quantity of competencies in the unit.</p></td>
+                    </tr>
+                    <tr>
+                        <td><a class="btn btn-block btn-default" href="/dashboards/operations/capability">Dash Two</a></td>
+                        <td><p>This dashboard displays information related to the gaps in member training according to required core competencies.</p></td>
+                    </tr>
+                </table>
             </div>
         </div>
     </div>
+    <!--End Logistics Panel-->
 </div>
 
 @endsection
