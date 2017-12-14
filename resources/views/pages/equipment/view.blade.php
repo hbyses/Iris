6f51e65f55753ce6f9ff7caaa2df013ccdabc5a5
Ross Wallace <ross.wallace@strat.is>
2017-12-14 13:30:38 +1100

    Updaing views for equipment

---
+++ resources/views/pages/equipment/view.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.pages')
+
+@section('content')
+<div class="panel panel-white">
+    <div class="panel-body">
+        <!--END DETAILS COLUMN-->
+        <div class="col-md-8">
+            <div class="table-responsive">
+            <h2>Chainsaw - 036 Stihl - 00005679</h2>
+            <table class="display table table-condensed" style="width: 100%; cellspacing: 0;">
+                <thead>
+                    <tr>
+                        <th>Property</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>ID</td>
+                        <td>00005679</td>
+                    </tr>
+                    <tr>
+                        <td>Name</td>
+                        <td>Chainsaw</td>
+                    </tr>
+                    <tr>
+                        <td>Brand</td>
+                        <td>Stihl</td>
+                    </tr>
+                    <tr>
+                        <td>Size</td>
+                        <td>036</td>
+                    </tr>
+                </tbody>
+                </table>  
+            </div>
+        </div>
+        <!--END DETAILS COLUMN-->
+    </div>
+</div>
+@endsection
